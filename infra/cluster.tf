4cc822687dc3a0b4ee1700ecb21d379d8ec2376c
Sourav Kundu <skundu.dev@gmail.com>
2024-03-26 13:31:36 +0530

    corrected resource name

--- infra/cluster.tf
+++ infra/cluster.tf
@@ -4,7 +4,7 @@ resource "aws_ecs_cluster" "app_cluster" {
   configuration {
     execute_command_configuration {
       logging    = "OVERRIDE"
-      kms_key_id = aws_kms_key.key.id
+      kms_key_id = aws_kms_key.app_4_key.id
       log_configuration {
         cloud_watch_encryption_enabled = true
         cloud_watch_log_group_name     = aws_cloudwatch_log_group.logs.name
