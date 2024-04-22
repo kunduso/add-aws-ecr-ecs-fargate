3b90238f64b75bbff6d0c914580ac1ef237c7f52
Sourav Kundu <skundu.dev@gmail.com>
2024-04-22 07:03:23 -0500

    CKV_AWS_25 and CKV_AWS_23

--- infra/network.tf
+++ infra/network.tf
@@ -41,15 +41,17 @@ resource "aws_security_group" "custom_sg" {
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
 resource "aws_security_group_rule" "ingress" {
+  description = "allow traffic into the load balancer"
   type              = "ingress"
-  from_port         = 0
-  to_port           = 65535
+  from_port         = 8080
+  to_port           = 8080
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.custom_sg.id
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
 resource "aws_security_group_rule" "egress" {
+  description = "allow traffic to reach outside the vpc"
   type              = "egress"
   from_port         = 0
   to_port           = 65535
