0b3030522ecb036c3d452d8e00f11c6d5221fd26
Sourav Kundu <skundu.dev@gmail.com>
2024-04-22 07:04:06 -0500

    format code

--- infra/network.tf
+++ infra/network.tf
@@ -35,13 +35,13 @@ resource "aws_security_group" "custom_sg" {
   name        = "${var.name}_allow_inbound_access"
   description = "allow inbound traffic"
   vpc_id      = aws_vpc.this.id
-    tags = {
+  tags = {
     "Name" = "${var.name}-sg"
   }
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
 resource "aws_security_group_rule" "ingress" {
-  description = "allow traffic into the load balancer"
+  description       = "allow traffic into the load balancer"
   type              = "ingress"
   from_port         = 8080
   to_port           = 8080
@@ -51,7 +51,7 @@ resource "aws_security_group_rule" "ingress" {
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
 resource "aws_security_group_rule" "egress" {
-  description = "allow traffic to reach outside the vpc"
+  description       = "allow traffic to reach outside the vpc"
   type              = "egress"
   from_port         = 0
   to_port           = 65535
