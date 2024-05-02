ea487b7d9b1202f0bfe0482de6ea36bbdc7a66c3
Sourav Kundu <skundu.dev@gmail.com>
2024-05-01 23:44:54 -0500

    #32 added outputs

---
+++ infra/output.tf
@@ -0,0 +1,15 @@
+output "public_subnet_ids" {
+  value = aws_subnet.public.*.id
+}
+output "security_group_id" {
+  value = aws_security_group.custom_sg.id
+}
+output "aws_lb_target_group" {
+  value = aws_lb_target_group.tg_blue.arn
+}
+output "cluster_name" {
+  value = aws_ecs_cluster.app_cluster.name
+}
+output "cloud_watch_log_group_name" {
+  value = aws_cloudwatch_log_group.logs.name
+}
