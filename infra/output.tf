0d804f74f1d319ed7582de2ece9f4cc713c94f24
Sourav Kundu <skundu.dev@gmail.com>
2024-05-01 23:48:54 -0500

    #32 corrected resource reference

--- infra/output.tf
+++ infra/output.tf
@@ -5,7 +5,7 @@ output "security_group_id" {
   value = aws_security_group.custom_sg.id
 }
 output "aws_lb_target_group" {
-  value = aws_lb_target_group.tg_blue.arn
+  value = aws_lb_target_group.target_group.arn
 }
 output "cluster_name" {
   value = aws_ecs_cluster.app_cluster.name
