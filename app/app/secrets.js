2de7dc1a631d140938e3aeea2ee724156294b455
Sourav Kundu <skundu.dev@gmail.com>
2024-06-29 22:18:46 -0500

    more fix for #72

--- app/app/secrets.js
+++ app/app/secrets.js
@@ -1,7 +1,14 @@
 const AWS = require('aws-sdk');
 
+let cachedSecretValue = null; // Variable to cache the secret value
+
 // Function to retrieve secret value from AWS Secrets Manager
 async function getSecretValue(secretName) {
+    if (cachedSecretValue !== null) {
+        // If cached secret value exists, return it immediately
+        return cachedSecretValue;
+    }
+
     const region = process.env.AWS_REGION; // Retrieve AWS region from environment variable
 
     // Configure AWS SDK with the retrieved region
@@ -15,18 +22,25 @@ async function getSecretValue(secretName) {
         if (data.SecretString) {
             try {
                 // Attempt to parse as JSON
-                return JSON.parse(data.SecretString);
+                cachedSecretValue = JSON.parse(data.SecretString);
             } catch (err) {
-                // If parsing as JSON fails, return the plain string value
-                return data.SecretString;
+                // If parsing as JSON fails, store as plain string
+                cachedSecretValue = data.SecretString;
             }
         } else {
             throw new Error("SecretString not found in AWS Secrets Manager response.");
         }
+
+        return cachedSecretValue; // Return the cached secret value
     } catch (err) {
         console.error("Error retrieving secret from AWS Secrets Manager:", err);
         throw err;
     }
 }
 
-module.exports = { getSecretValue };
+// Function to clear the cached secret value
+function clearCache() {
+    cachedSecretValue = null;
+}
+
+module.exports = { getSecretValue, clearCache };
