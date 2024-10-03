c7e927c8a625296b06841bab9757339ea3e32704
Sourav Kundu <skundu.dev@gmail.com>
2024-10-03 06:55:37 -0500

    #82 updated listener with blue target group

--- infra/load_balancer.tf
+++ infra/load_balancer.tf
@@ -44,10 +44,9 @@ resource "aws_alb_listener" "listener" {
   load_balancer_arn = aws_lb.app_lb.id
   port              = 80
   protocol          = "HTTP"
-
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.target_group.arn
+    target_group_arn = aws_lb_target_group.blue_target_group.arn
   }
   lifecycle {
     ignore_changes = [default_action]
