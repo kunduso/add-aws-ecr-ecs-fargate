27d805723fdf3d46c222cdb69a9a0308321fe3c0
Sourav Kundu <skundu.dev@gmail.com>
2024-10-03 07:05:50 -0500

    #82 comment out. Will fix later

--- deploy/code_deploy_role.tf
+++ deploy/code_deploy_role.tf
@@ -1,70 +1,70 @@
-data "aws_iam_policy_document" "assume_by_codedeploy" {
-  statement {
-    sid     = ""
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
+# data "aws_iam_policy_document" "assume_by_codedeploy" {
+#   statement {
+#     sid     = ""
+#     effect  = "Allow"
+#     actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["codedeploy.amazonaws.com"]
-    }
-  }
-}
+#     principals {
+#       type        = "Service"
+#       identifiers = ["codedeploy.amazonaws.com"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "codedeploy" {
-  name               = "codedeploy"
-  assume_role_policy = data.aws_iam_policy_document.assume_by_codedeploy.json
-}
+# resource "aws_iam_role" "codedeploy" {
+#   name               = "codedeploy"
+#   assume_role_policy = data.aws_iam_policy_document.assume_by_codedeploy.json
+# }
 
 
-data "aws_iam_policy_document" "codedeploy" {
-  statement {
-    sid    = "AllowLoadBalancingAndECSModifications"
-    effect = "Allow"
+# data "aws_iam_policy_document" "codedeploy" {
+#   statement {
+#     sid    = "AllowLoadBalancingAndECSModifications"
+#     effect = "Allow"
 
-    actions = [
-      "ecs:CreateTaskSet",
-      "ecs:DeleteTaskSet",
-      "ecs:DescribeServices",
-      "ecs:UpdateServicePrimaryTaskSet",
-      "elasticloadbalancing:DescribeListeners",
-      "elasticloadbalancing:DescribeRules",
-      "elasticloadbalancing:DescribeTargetGroups",
-      "elasticloadbalancing:ModifyListener",
-      "elasticloadbalancing:ModifyRule",
-      "s3:GetObject"
-    ]
+#     actions = [
+#       "ecs:CreateTaskSet",
+#       "ecs:DeleteTaskSet",
+#       "ecs:DescribeServices",
+#       "ecs:UpdateServicePrimaryTaskSet",
+#       "elasticloadbalancing:DescribeListeners",
+#       "elasticloadbalancing:DescribeRules",
+#       "elasticloadbalancing:DescribeTargetGroups",
+#       "elasticloadbalancing:ModifyListener",
+#       "elasticloadbalancing:ModifyRule",
+#       "s3:GetObject"
+#     ]
 
-    resources = ["*"]
-  }
-  statement {
-    sid    = "AllowPassRole"
-    effect = "Allow"
+#     resources = ["*"]
+#   }
+#   statement {
+#     sid    = "AllowPassRole"
+#     effect = "Allow"
 
-    actions = ["iam:PassRole"]
+#     actions = ["iam:PassRole"]
 
-    resources = [
-      aws_iam_role.ecs_task_execution_role.arn
-    ]
-  }
+#     resources = [
+#       aws_iam_role.ecs_task_execution_role.arn
+#     ]
+#   }
 
-  statement {
-    sid    = "DeployService"
-    effect = "Allow"
+#   statement {
+#     sid    = "DeployService"
+#     effect = "Allow"
 
-    actions = [
-      "ecs:DescribeServices",
-      "codedeploy:GetDeploymentGroup",
-      "codedeploy:CreateDeployment",
-      "codedeploy:GetDeployment",
-      "codedeploy:GetDeploymentConfig",
-      "codedeploy:RegisterApplicationRevision"
-    ]
+#     actions = [
+#       "ecs:DescribeServices",
+#       "codedeploy:GetDeploymentGroup",
+#       "codedeploy:CreateDeployment",
+#       "codedeploy:GetDeployment",
+#       "codedeploy:GetDeploymentConfig",
+#       "codedeploy:RegisterApplicationRevision"
+#     ]
 
-    resources = ["*"]
-  }
-}
-resource "aws_iam_role_policy" "codedeploy" {
-  role   = aws_iam_role.codedeploy.name
-  policy = data.aws_iam_policy_document.codedeploy.json
-}
+#     resources = ["*"]
+#   }
+# }
+# resource "aws_iam_role_policy" "codedeploy" {
+#   role   = aws_iam_role.codedeploy.name
+#   policy = data.aws_iam_policy_document.codedeploy.json
+# }
