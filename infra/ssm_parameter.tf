acb7388240981e4ad8360a9cb1657292920f60ac
Sourav Kundu <skundu.dev@gmail.com>
2024-10-03 06:56:01 -0500

    #82 exporting values for downstream deployment

--- infra/ssm_parameter.tf
+++ infra/ssm_parameter.tf
@@ -7,10 +7,16 @@ resource "aws_ssm_parameter" "infra_output" {
   value = jsonencode({
     "subnet_ids" : [for subnet in aws_subnet.private : subnet.id],
     "security_group_id" : "${aws_security_group.custom_sg.id}",
-    "aws_lb_target_group" : "${aws_lb_target_group.target_group.arn}",
-    "cluster_id" : "${aws_ecs_cluster.app_cluster.id}",
-    "cloud_watch_log_group_name" : "${aws_cloudwatch_log_group.logs.name}"
-    "secret_arn" : "${aws_secretsmanager_secret.ecs_secret.arn}"
+    "aws_lb_blue_target_group" : "${aws_lb_target_group.blue_target_group.arn}",
+    "aws_lb_green_target_group" : "${aws_lb_target_group.green_target_group.arn}",
+    "aws_lb_listener" : "${aws_alb_listener.listener.arn}",
+    "aws_lb" : "${aws_lb.app_lb.arn}",
+    "aws_vpc_id" : "${aws_vpc.this.id}",
+    "aws_ecs_cluster_id" : "${aws_ecs_cluster.app_cluster.id}",
+    "aws_ecs_cluster_name" : "${aws_ecs_cluster.app_cluster.name}",
+    "aws_cloudwatch_log_group" : "${aws_cloudwatch_log_group.logs.arn}",
+    "aws_cloudwatch_log_group_name" : "${aws_cloudwatch_log_group.logs.name}",
+    "secret_arn" : "${aws_secretsmanager_secret.ecs_secret.arn}",
     "kms_arn" : "${aws_kms_key.custom_kms_key.arn}"
   })
 }
