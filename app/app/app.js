da1d1bd03fddd95c0e478ef969607e955a0cd50e
sourav kundu <kunduso@amazon.com>
2025-06-05 00:08:10 -0500

    updated comments

--- app/app/app.js
+++ app/app/app.js
@@ -13,7 +13,7 @@ app.use(morgan('combined'));
 app.use('/healthcheck', require('./route/healthcheck'));
 
 const port = process.env.PORT || 8080;
-const message = process.env.MESSAGE || "Hello from the Docker container!";
+const message = process.env.MESSAGE || "Hello AWS Midwest Community Day from the Docker container!";
 const secretName = 'ecs_secret'; // Name of your AWS Secrets Manager secret
 
 // Retrieve secret value asynchronously
